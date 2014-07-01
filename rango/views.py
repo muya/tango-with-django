61d62b954c568096cb3eeac8f2c367db363e2925
muya <kingkonig@gmail.com>
2014-07-01 18:42:32 +0300

    using templates
    -added templates directory
    -defined templates directory in settings
    -added main template

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("Rango says hello world!")
+	context = RequestContext(request)
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	return render_to_response('rango/index.html', context_dict, context)
+
+def about(request):
+	page_content = "<a href='/rango/'>Home</a><br>Rango Says: Here is the about page"
+	return HttpResponse(page_content)
